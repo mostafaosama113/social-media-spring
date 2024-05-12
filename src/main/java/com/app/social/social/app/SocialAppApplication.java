eab644e02bf8ab34115c0c0886870b52ff5c2e7d
mostafa osama <hitmanx97@gmail.com>
2024-05-12 12:35:45 +0300

    add data for testing

--- src/main/java/com/app/social/social/app/SocialAppApplication.java
+++ src/main/java/com/app/social/social/app/SocialAppApplication.java
@@ -1,7 +1,11 @@
 package com.app.social.social.app;
 
+import com.app.social.social.app.entity.Post;
+import com.app.social.social.app.repositry.PostRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +14,18 @@ public class SocialAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialAppApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(PostRepository postRepository) {
+		return args -> {
+			for(int i = 0 ; i < 100 ; i++){
+				Post post = new Post();
+				post.setTitle("Title " + i);
+				post.setContent("Content " + i);
+				postRepository.save(post);
+			}
+		};
 	}
 
 }
